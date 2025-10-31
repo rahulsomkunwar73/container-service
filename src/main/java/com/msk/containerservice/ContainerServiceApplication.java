09012bc0690a74ddefbceb457a2bf71ee212053a
rsomkunwar <rahul.somkunwar@cj.com>
2025-10-31 23:31:53 +0530

    first commit with initial project structure

---
+++ src/main/java/com/msk/containerservice/ContainerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.msk.containerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msk/containerservice/ContainerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msk.containerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
